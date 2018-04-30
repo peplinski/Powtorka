6df28e45851043c48c85dac2d7ced978c5d223c2
Driver <peplinskikrzysztof@gmail.com>
2018-04-30 12:05:07 +0200

    first commit

---
+++ src/main/java/pl/com/powtorka/ksztalty/Choinka.java
@@ -0,0 +1,47 @@
+package pl.com.powtorka.ksztalty;
+
+public class Choinka {
+
+
+    public static void main(String[] args) {
+
+        //choinka
+        int liczba = 10;
+        for (int i = 0; i <= liczba; i++) {
+
+            for (int j = i; j < liczba; j++) {
+                System.out.print(" ");
+            }
+
+            for (int z = 0; z <= i - 1; z++)
+                System.out.print("*");
+            System.out.print("");
+
+            for (int k = liczba - i; k < liczba; k++)
+                System.out.print("*");
+            System.out.println();
+
+        }
+        System.out.println();
+
+//prostokat
+        int wysokosc = 5;
+        for (int i = 0; i <= wysokosc; i++) {
+            System.out.print(" ");
+            for (int j = 0; j < wysokosc; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        //trojkat
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+        }
+    }
+}
+
